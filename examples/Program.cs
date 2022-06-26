e3ed844e9f1907b22f2ee71ab00790f907aeb762
Bairam Mukhiev <Muhiyev@bk.ru>
2022-06-26 19:14:51 +0300

    new rondom

--- examples/Program.cs
+++ examples/Program.cs
@@ -1,6 +1,8 @@
-﻿int numberA = 50;
-int numberB =5;
-int result = (numberA/numberB);
+﻿int numberA = new Random().Next (1,10);
+Console.WriteLine(numberA);
+int numberB =new Random().Next (1,10);;
+Console.WriteLine(numberB);
+int result = (numberA+numberB);
 Console.WriteLine (result);
 
 double numberD = 50;
